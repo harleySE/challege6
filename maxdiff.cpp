3f6dde6d80fdc7b06c0814ae15f0f04b0f89a91c
harleySE <72030467+harleySE@users.noreply.github.com>
2020-10-15 09:46:52 -0500

    Update maxdiff.cpp

--- maxdiff.cpp
+++ maxdiff.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int a;
-  int b;
+  int a=0;
   int c=0;
-  int d,e,f,g,h,i,j,k,l,m;
+  int d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0;
   cout<<"enter 10 numbers"<<endl;
   cin>>d>>e>>f>>g>>h>>i>>j>>k>>l>>m;
   int arr[]={d,e,f,g,h,i,j,k,l,m};
   int arrSize=sizeof(arr)/sizeof(arr[0]);
-  for(int j=arrSize; j>0; j--){
-    for(int i=0; i<arrSize; i++){
-      if(arr[i]>arr[i-1]){
-        a=arr[i];
-      }
-      else if(arr[i]<arr[i-1]){
-        b=arr[i];
-      }
-      if(abs(a-b)>c){
-        c=abs(a-b);
-      }
+  int b=arr[0];
+  for(int i=0; i<arrSize; i++){
+    if(arr[i]>a){
+      a=arr[i];
+    }
+    else if(arr[i]<b){
+      b=arr[i];
+    }
+    if(abs(a-b)>c){
+      c=abs(a-b);
     }
   }
   cout<<"the largest absolute value is "<<c<<endl;

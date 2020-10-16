ec399638ad838227237c21fd9704f4fc78a85b02
harleySE <72030467+harleySE@users.noreply.github.com>
2020-10-16 09:39:47 -0500

    Update progress day.cpp

--- progress day.cpp
+++ progress day.cpp
@@ -9,6 +9,9 @@ int main() {
   int arr[]={d,e,f,g,h};
   int arrSize=sizeof(arr)/sizeof(arr[0]);
   for(int i=0; i<arrSize; i++){
+    if(i-1<0){
+      continue;
+    }
     if(arr[i]<arr[i-1]){
       pd+=1;
     }
